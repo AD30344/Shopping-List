f070a4a10a1cdb2932bd536ca6ea060b953f5720
Aaron Davis <63382332+AD30344@users.noreply.github.com>
2020-08-11 22:48:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function main() {
+    $('form').submit(function (event) {
+        event.preventDefault();
+
+
+
+
+        //takes input text and then add items to beginning of list
+
+        let inputText = $(this).find('input').val();
+
+        $('li').first('li').before(`<li><span class="shopping-item">${inputText}
+        </span><div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+        </button><button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+        </button></div></li>`);
+
+    });
+
+
+
+
+    //toggles class with check button
+    $('.shopping-list').on('click', '.shopping-item-toggle', function () {
+
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+
+
+
+    //deletes items are render
+
+    $('.shopping-list').on('click', 'li', function () {
+        $(this).closest('li').remove();
+    })
